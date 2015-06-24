29361eb5e6ac936a8f5b34d19de1a0db444ed3de
Mauricio Arce <mauricio.arce.mat@gmail.com>
2015-06-23 23:51:45 -0400

    Initial commit

---
+++ HelloWorld/app/src/main/java/com/example/mauricioarce/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mauricioarce.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mauricio Arce on 20/06/2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.main_activity);
+    }
+}
